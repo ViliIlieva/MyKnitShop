01a448db66525b7517d2c96d6e721bcc21deafbe
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2023-02-22 13:32:11 +0200

    Add files via upload

---
+++ src/java/com/example/myknitshop/MyKnitShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.myknitshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKnitShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyKnitShopApplication.class, args);
+    }
+
+}
