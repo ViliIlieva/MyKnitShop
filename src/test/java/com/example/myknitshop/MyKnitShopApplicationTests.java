d09cd73ec19df0779884a585e739f08860cf4d59
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2023-02-22 13:30:48 +0200

    Add files via upload

---
+++ src/test/java/com/example/myknitshop/MyKnitShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myknitshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyKnitShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
