d963ae1e9d002ad7da4518105209d29b1689a5df
ViliIlieva <levi@abv.bg>
2023-03-21 22:02:20 +0200

    ready with implement REST Api

--- src/main/resources/static/js/comments.js
+++ src/main/resources/static/js/comments.js
@@ -2,22 +2,12 @@
 let commentSection = document.getElementById('comment')
 
 fetch(`http://localhost:8080/api/comments`)
-    .then((responsе) => responsе.json())
+    .then((resp) => resp.json())
     .then(json => json.forEach(message => {
-        let commentHtml = '<div class="comment">\n'
-        commentHtml += `<p>${message.description}</p>\n`
-        commentHtml += `<h4>${message.author.firstName}</h4>\n`
+        let commentHtml = '<div class="comment text-center">\n'
+        commentHtml += `<h6>${message.authorFullName}</h6>\n`
+        commentHtml += `<h6 class="mb-4">${message.description}</h6>\n`
         commentHtml += '</div>\n'
 
         commentSection.innerHTML += commentHtml
     }))
-    // .then((body) => {
-    //     for (comment of body) {
-    //         let commentHtml = '<div class="comment">\n'
-    //         commentHtml += `<p>${comment.description}</p>\n`
-    //         commentHtml += `<h4>${comment.author.firstNamame}</h4>\n`
-    //         commentHtml += '</div>\n'
-    //
-    //         commentSection.innerHTML += commentHtml
-    //     }
-    // })
