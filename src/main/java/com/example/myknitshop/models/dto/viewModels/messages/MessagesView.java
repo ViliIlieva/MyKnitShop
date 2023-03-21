d7e1c5a18a0db1e33d4c823295d4b287b288aea2
ViliIlieva <levi@abv.bg>
2023-03-21 20:42:41 +0200

    edit MessagesView.class

--- src/main/java/com/example/myknitshop/models/dto/viewModels/messages/MessagesView.java
+++ src/main/java/com/example/myknitshop/models/dto/viewModels/messages/MessagesView.java
@@ -7,35 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class MessagesView {
-    private Long id;
     private String description;
     private String authorFullName;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public MessagesView setDescription(String description) {
-        this.description = description;
-        return this;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public MessagesView setId(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getAuthorFullName() {
-        return authorFullName;
-    }
-
-    public MessagesView setAuthorFullName(String authorFullName) {
-        this.authorFullName = authorFullName;
-        return this;
-    }
 }
