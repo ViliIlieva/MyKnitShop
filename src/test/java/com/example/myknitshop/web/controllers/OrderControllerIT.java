68c84dce9fc146b0a3e2c7487692fbade31925f6
ViliIlieva <levi@abv.bg>
2023-03-27 21:49:26 +0300

    50% test line coverage

--- src/test/java/com/example/myknitshop/web/controllers/OrderControllerIT.java
+++ src/test/java/com/example/myknitshop/web/controllers/OrderControllerIT.java
@@ -1,5 +1,6 @@
 package com.example.myknitshop.web.controllers;
 
+import com.example.myknitshop.models.dto.bindingModels.messages.MessageDTO;
 import com.example.myknitshop.models.dto.viewModels.products.ProductViewInCart;
 import com.example.myknitshop.models.entity.ChoseProducts;
 import com.example.myknitshop.models.entity.Order;
@@ -9,7 +10,10 @@ import com.example.myknitshop.repository.ChoseProductsRepository;
 import com.example.myknitshop.repository.UserRepository;
 import com.example.myknitshop.service.OrderService;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -33,47 +37,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class OrderControllerIT {
     @MockBean
     private OrderService orderService;
-    @Mock
-    private UserRepository userRepository;
-    @Mock
-    private ChoseProductsRepository choseProductsRepository;
+
     @Autowired
     private MockMvc mockMvc;
-
-    private Order order;
-
-    @BeforeAll
-    void setUp(){
-        order = Order.builder()
-                .dateOrdered (LocalDate.now ())
-                .orderStatus (OrderStatusEnum.OPEN)
-                .build();
-
-        ProductViewInCart productViewInCart = ProductViewInCart.builder()
-                .id(1L)
-                .price(BigDecimal.valueOf(35))
-                .name("testProduct")
-                .productSum (BigDecimal.valueOf(35))
-                .quantity(1)
-                .build();
-        ChoseProducts choseProducts = ChoseProducts.builder()
-                .name("testProduct")
-                .price(BigDecimal.valueOf(35))
-                .quantity(1)
-                .productSum (BigDecimal.valueOf(35))
-                .build();
-        choseProductsRepository.save(choseProducts);
-
-        User client = User.builder()
-                .username("client")
-                .password("client")
-                .firstName("Client")
-                .lastName("Client")
-                .choseProduct(List.of(choseProducts))
-                .email("x@test")
-                .build();
-        userRepository.save(client);
-    }
 
     @Test
     @WithMockUser(username = "client", roles = {"CLIENT"})
@@ -100,12 +66,9 @@ public class OrderControllerIT {
     @Test
     @WithMockUser(username = "admin", roles = {"ADMIN"})
     void testCloseOrderById() throws Exception {
-        when(orderService.closeOrder (1L)).thenReturn(createComment("Test comment."));
-        when(orderService.closeOrder (1L)).thenReturn (order.setOrderStatus (OrderStatusEnum.COMPLETED));
-
     mockMvc.perform (get ("/order/close/1"))
                 .andExpect (status ()
-                        .is2xxSuccessful ())
+                        .is3xxRedirection ())
                 .andExpect (redirectedUrl ("/user/admin"));
     }
 }
