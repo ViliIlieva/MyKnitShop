cbffe372d7a9100f522726bce42993eee389437d
ViliIlieva <levi@abv.bg>
2023-03-28 15:25:21 +0300

    54% test line coverage

--- src/test/java/com/example/myknitshop/service/UserServiceTest.java
+++ src/test/java/com/example/myknitshop/service/UserServiceTest.java
@@ -1,29 +1,31 @@
 package com.example.myknitshop.service;
 
+import com.example.myknitshop.models.dto.bindingModels.messages.MessageDTO;
 import com.example.myknitshop.models.dto.viewModels.orders.OrderDetailView;
 import com.example.myknitshop.models.dto.viewModels.users.AllUsersView;
-import com.example.myknitshop.models.entity.Order;
-import com.example.myknitshop.models.entity.Role;
-import com.example.myknitshop.models.entity.User;
+import com.example.myknitshop.models.entity.*;
 import com.example.myknitshop.models.enums.UserRoleEnum;
 import com.example.myknitshop.repository.MessageRepository;
 import com.example.myknitshop.repository.OrderRepository;
 import com.example.myknitshop.repository.UserRepository;
-import com.example.myknitshop.repository.UserRoleRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import java.math.BigDecimal;
+import java.security.Principal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -32,71 +34,67 @@ public class UserServiceTest {
     private final String ENCODED_PASSWORD = "%($)GGPPP3fdfd";
 
     private final Long VALID_ID = 1L;
-    private final Long INVALID_ID = 300L;
-
     private final String FIRST_NAME = "Plamena";
     private final String LAST_NAME = "Plamenova";
     private final String EMAIL = "plamena@example.com";
-    private final String NON_EXISTING_USERNAME = "plami";
-
-    private UserService toTest;
 
     @Mock
     private UserRepository mockUserRepository;
     @Mock
-    private ModelMapper mockMapper;
+    private MessageRepository mockMessageRepository;
     @Mock
     private OrderRepository mockOrderRepository;
     @Mock
-    private MessageRepository mockMessageRepository;
+    private ModelMapper mockMapper;
     @Mock
     private OrderService mockOrderService;
     @Mock
-    private ProductService mockProductService;
-    @Mock
-    private ChoseProductsService mockChoseProductsService;
-    @Mock
-    private PurchasedProductsService mockPurchasedProductsService;
-    @Mock
-    private UserRoleRepository mockUserRoleRepository;
-    @Mock
     private PasswordEncoder mockPasswordEncoder;
+    @Mock
+    private User user;
+    @Mock
+    Principal principal;
+    @Mock
+    Message message;
+    @Spy
+    @InjectMocks
+    private UserService toTest;
+
+    @Captor
+    private ArgumentCaptor<Message> messageCaptor;
+    @Captor
+    private ArgumentCaptor<User> userCaptor;
+    @Captor
+    private ArgumentCaptor<Order> orderCaptor;
 
     private AllUsersView allUsersView;
 
-    private User testUser;
-    private Order testOrder;
-    private OrderDetailView testOrderDetailView;
+    private Order order;
+    private MessageDTO messageDTO;
+    private OrderDetailView orderDetailView;
 
     private Role testRole;
 
     @BeforeEach
     void setUp() {
-        toTest = new UserService (
-                mockMapper, mockUserRepository,
-                mockProductService, mockOrderRepository,
-                mockMessageRepository, mockOrderService,
-                mockChoseProductsService, mockPurchasedProductsService,
-                mockUserRoleRepository
-
-        );
-
         testRole = new Role ();
         testRole.setUserRole (UserRoleEnum.CLIENT);
 
-        testUser = new User ()
-                .setUsername (NEW_USERNAME)
-                .setPassword (mockPasswordEncoder.encode (RAW_PASSWORD))
-                .setFirstName (FIRST_NAME)
-                .setLastName (LAST_NAME)
-                .setEmail (EMAIL);
+       user = User .builder()
+               .username(NEW_USERNAME)
+               .password(mockPasswordEncoder.encode (RAW_PASSWORD))
+               .firstName(FIRST_NAME)
+               .lastName(LAST_NAME)
+               .email(EMAIL)
+               .messages(List.of( message))
+               .build();
 
-        testOrder = Order.builder ()
-                .client (testUser)
+        order = Order.builder ()
+                .client (user)
                 .dateOrdered (LocalDate.now ())
                 .build ();
 
-        testOrderDetailView = new OrderDetailView ();
+        orderDetailView = new OrderDetailView ();
 
         allUsersView = AllUsersView.builder ()
                 .id (VALID_ID)
@@ -104,27 +102,61 @@ public class UserServiceTest {
                 .email (EMAIL)
                 .roles (List.of (testRole))
                 .build ();
+        message = Message.builder()
+                .author(user)
+                .description("Test Message Test Message Test Message")
+                .build();
+        messageDTO = MessageDTO.builder()
+                .orderId(1)
+                .description("Test Message Test Message Test Message")
+                .build();
 
+        lenient().when(mockUserRepository.findByUsername(NEW_USERNAME)).thenReturn(Optional.of(user));
+        Mockito.<Optional<User>>when(mockUserRepository.findByUsername(NEW_USERNAME)).thenReturn(Optional.of(user));
+        lenient().when(principal.getName()).thenReturn(NEW_USERNAME);
+        lenient().when((toTest.getUserByPrincipal(principal))).thenReturn(user);
+
+
+    }
+
+//    @Test
+//    void testRemoveProductFromChoseList(){
+//        when(mockUserRepository.findByUsername(NEW_USERNAME)).thenReturn(Optional.of(testUser));
+//        Mockito.<Optional<User>>when(mockUserRepository.findByUsername(NEW_USERNAME)).thenReturn(Optional.of(testUser));
+//        when(principal.getName()).thenReturn(NEW_USERNAME);
+//        when((toTest.getUserByPrincipal(principal))).thenReturn(testUser);
+//
+//        toTest.removeProductFromChoseList(choseProducts.getId(), principal);
+//    }
+    @Test
+    @WithMockUser(username = "client", roles = {"CLIENT"})
+    void testAddMessage(){
+        when(mockOrderService.findById(VALID_ID)).thenReturn(order);
+        toTest.addMessage(messageDTO, principal);
+
+        verify(mockMessageRepository, times(1)).save(message);
+        verify(mockUserRepository, times(1)).save(user);
+        verify(mockOrderRepository, times(1)).save(order);
     }
 
     @Test
     void testGetAllUsersFromRepo() {
-        lenient ().when (mockUserRepository.findAll ()).thenReturn (List.of (testUser));
-        lenient ().when (mockMapper.map (testUser, AllUsersView.class)).thenReturn (allUsersView);
+        lenient ().when (mockUserRepository.findAll ()).thenReturn (List.of (user));
+        lenient ().when (mockMapper.map (user, AllUsersView.class)).thenReturn (allUsersView);
 
         AllUsersView allUsersView1 = toTest.getAllUsers ().get (0);
 
         Assertions.assertEquals (allUsersView1.getUsername (), (allUsersView.getUsername ()));
 
-        Assertions.assertEquals (testUser.getUsername (), allUsersView.getUsername ());
+        Assertions.assertEquals (user.getUsername (), allUsersView.getUsername ());
     }
 
     @Test
     void testGetOrderDetailsById(){
-        lenient ().when (mockMapper.map (testOrder, OrderDetailView.class)).thenReturn (testOrderDetailView);
-        testOrderDetailView.setClientAddress ("гр.Варна ул.Тестова №3");
-        testOrderDetailView.setClientFirstName (FIRST_NAME + " " + LAST_NAME);
-        testOrderDetailView.setOrderSum (BigDecimal.valueOf (75));
-        testOrderDetailView.setClientFullName (FIRST_NAME);
+        lenient ().when (mockMapper.map (order, OrderDetailView.class)).thenReturn (orderDetailView);
+        orderDetailView.setClientAddress ("гр.Варна ул.Тестова №3");
+        orderDetailView.setClientFirstName (FIRST_NAME + " " + LAST_NAME);
+        orderDetailView.setOrderSum (BigDecimal.valueOf (75));
+        orderDetailView.setClientFullName (FIRST_NAME);
     }
 }
